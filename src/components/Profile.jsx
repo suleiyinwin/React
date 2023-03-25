9d0b54d95f944d46cbf7ff52c960b20f3d662979
Nan Zun La Pyae <nanzun.lapy@kmutt.ac.th>
2023-03-25 23:21:30 +0700

    Checkpt 1,2,3

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/Nan.jpg" alt="profile/"></img>
+            <h1 className="name">Nan Zun La Pyae</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
