52fe2250ca8b5c55da7e6b293530c34ba5805d19
Su Lei Yin Win <yunluo.2106@gmail.com>
2023-03-26 10:17:59 +0700

    commit 1 lab

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,8 +2,8 @@ import "./Profile.css";
 function Profile(){
     return(
         <div className="layout">
-            <img src="assets/Nan.jpg" alt="profile/"></img>
-            <h1 className="name">Nan Zun La Pyae</h1>
+            <img src="assets/IMG_4069.jpg" alt="profile/"></img>
+            <h1 className="name">Larry</h1>
             <hr></hr>
         </div>
     )
